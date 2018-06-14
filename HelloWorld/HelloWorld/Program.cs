06c6331da9ee88219dac777b2b39e532642c530e
Unknown <zsugarhell@gmail.com>
2018-06-14 16:29:30 +0300

    1.2
    Added another line

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Please learn git");
+            Console.WriteLine("Please no");
         }
     }
 }
